8404a0ce735625213ddabadb3409733b7ec2d68d
lunzhang <lun.zhang1993@gmail.com>
2017-10-26 14:54:24 -0400

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": "airbnb-base",
+  "rules": {
+    "no-mixed-operators": 0,
+    "no-param-reassign": 0,
+    "no-plusplus": [2, {"allowForLoopAfterthoughts": true}],
+    "import/no-extraneous-dependencies": 0,
+    "max-len": [2, {"code": 160}],
+    "import/no-unresolved": 0,
+    "import/extensions": 0,
+    "default-case": 0,
+    "space-infix-ops": ["error", {"int32Hint": false}]
+  }
+};
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = [{
+  entry: {
+    'openar': './src/index.js',
+    'openar.min': './src/index.js'
+  },
+  output: {
+    libraryTarget: 'umd',
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js"
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+    ],
+  },
+  devServer: {
+    publicPath: '/dist',
+    contentBase: [path.join(__dirname, "examples")],
+    inline: true,
+    port: 8080
+  },
+  plugins: [
+    new UglifyJSPlugin({
+      test: /\.min\.js$/
+    })
+  ]
+}];
