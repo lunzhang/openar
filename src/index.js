dfd91b9482bf789d8d763d7550ea70351037441a
lunzhang <lun.zhang1993@gmail.com>
2017-10-27 16:28:54 -0400

    [change] render of image in example basic_setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
+export default class OpenAR {
+  constructor(scene) {
+    this.scene = scene;
+    this.init();
+  }
 
+  /**
+  * Get user camera using video
+  * Add video texture to scene
+  **/
+  init() {
+    // video element
+    this.video = document.createElement('video');
+
+    // get user camera and attach to video element
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        this.video.src = window.URL.createObjectURL(stream);
+        this.video.play();
+      });
+    }
+
+    // this.videoTexture = new THREE.VideoTexture(this.video);
+    // this.videoTexture.minFilter = THREE.LinearFilter;
+    // this.videoTexture.magFilter = THREE.LinearFilter;
+    //
+    // const material = new THREE.SpriteMaterial({ map: this.videoTexture });
+
+    // testing without webcam
+    var map = new THREE.TextureLoader().load( "./pulpitrock.jpg" );
+    var material = new THREE.SpriteMaterial({ map: map });
+
+    this.screen = new THREE.Sprite(material);
+    this.scene.add(this.screen);
+  }
+}
+
+global.OpenAR = OpenAR;
