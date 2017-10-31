7393893d6201f56c8a3951eee823df96da82a89c
lunzhang <lun.zhang1993@gmail.com>
2017-10-31 13:20:28 -0400

    [feature] add build script

---
+++ build.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+// remove current copy of file
+try {
+  fs.unlinkSync(path.resolve(__dirname, 'docs/openar.js'));
+}
+catch (err){
+  console.log(err);
+}
+
+// copy dist copy into docs
+try {
+  fs.createReadStream(path.resolve(__dirname, 'dist/openar.js'))
+  .pipe(fs.createWriteStream(path.resolve(__dirname, 'docs/openar.js')));
+}
+catch (err){
+  console.log(err);
+}
