2c1e8c17031cf59172f6fce6c97ceed25bc54d67
lunzhang <lun.zhang1993@gmail.com>
2017-11-28 12:56:40 -0500

    [feature] add recoverPose function

--- src/utils/motionEstimation.js
+++ src/utils/motionEstimation.js
@@ -18,42 +18,83 @@ function motionEstimation(prevFrame, currentFrame, width, height) {
     currentPyramidT.build(currentPyramidT.data[0], false);
     prevPyramidT.build(prevPyramidT.data[0], false);
 
-    // initialize previous and current frame features
     // detect features for previous frame only using fast algorithm
-    const currentCorners = [];
-    const prevCorners = [];
+    const prevCornersTemp = [];
     for (let i = 0; i < width * height; i++) {
-        prevCorners[i] = new jsfeat.keypoint_t();
+        prevCornersTemp[i] = new jsfeat.keypoint_t();
     }
-    const featuresCount = jsfeat.fast_corners.detect(prevPyramidT.data[0], prevCorners, 3);
+    const featuresCount = jsfeat.fast_corners.detect(prevPyramidT.data[0], prevCornersTemp, 3);
 
     // convert detected frames to array format
-    const prevCornersArray = [];
+    const prevCorners = [];
+    const currentCorners = [];
     for (let i = 0; i < featuresCount; i++) {
-        prevCornersArray.push(prevCorners[i].x);
-        prevCornersArray.push(prevCorners[i].y);
+        prevCorners.push(prevCornersTemp[i].x);
+        prevCorners.push(prevCornersTemp[i].y);
     }
 
     // klt tracker - tracks features in previous img and maps them to current
     const status = [];
     jsfeat.optical_flow_lk.track(prevPyramidT, currentPyramidT,
-    prevCornersArray, currentCorners, featuresCount,
+    prevCorners, currentCorners, featuresCount,
     15, 30, status, 0.01, 0.0001);
 
-    // this class allows you to use above Motion Kernels
-    // to estimate motion even with wrong correspondences
-    var ransac = jsfeat.motion_estimator.ransac;
+    // estimate motion even with wrong correspondences
+    const ransac = jsfeat.motion_estimator.ransac;
 
     // create homography kernel
-    // you can reuse it for different point sets
-    var homo_kernel = new jsfeat.motion_model.homography2d();
-    var transform = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
+    const homo_kernel = new jsfeat.motion_model.homography2d();
+    const essentialMatrix = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
+    const params = new jsfeat.ransac_params_t(4, 3, 0.5, 0.99);
 
-    var params = new jsfeat.ransac_params_t(4, 3, 0.5, 0.99);
+    // calculate essential matrix using features detected in the images
+    ransac(params, homo_kernel, prevCorners, currentCorners, featuresCount, essentialMatrix, status, 1000);
 
-    var ok = ransac(params, homo_kernel, prevCornersArray, currentCorners, featuresCount, transform, status, 1000);
+    const rotation = [];
+    const translation = [];
+    recoverPose(essentialMatrix, currentCorners, prevCorners, rotation, translation);
 
-    return transform;
+    return {
+        rotation,
+        translation,
+    };
+};
+
+// calculates the rotation and translation using essentialMatrix
+function recoverPose(essentialMatrix, currentCorners, prevCorners, rotation, transformation) {
+    const D = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
+    const U = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
+    const Ut = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
+    const V = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
+    const W = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
+    W.data[1] = -1;
+    W.data[3] = 1;
+    W.data[8] = 1;
+
+    // get inverse of W
+    const Winvert = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
+    jsfeat.linalg.svd_invert(Winvert, W);
+
+    // rotation and translation matrices
+    const R = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
+    const T = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
+
+    // calculate SVD of essential matrix
+    jsfeat.linalg.svd_decompose(essentialMatrix, D, U, V);
+
+    jsfeat.matmath.transpose(Ut, U);
+
+    // R = U * Winvert * V
+    jsfeat.matmath.multiply_3x3(R, U, Winvert);
+    jsfeat.matmath.multiply_3x3(R, R, V);
+
+    // T = U * W * D * Ut
+    jsfeat.matmath.multiply_3x3(T, U, W);
+    jsfeat.matmath.multiply_3x3(T, T, D);
+    jsfeat.matmath.multiply_3x3(T, T, Ut);
+
+    rotation = R.data;
+    translation = T.data;
 };
 
 export default motionEstimation;
