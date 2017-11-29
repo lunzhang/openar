308b6abad49573dd2f4462f9f7aa8e6822c9a112
lunzhang <lun.zhang1993@gmail.com>
2017-11-29 15:49:39 -0500

    [change] fix linting problems

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,21 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "env": {
-    "browser": true,
-    "es6": true
-  },
-  "extends": "airbnb-base",
-  "rules": {
-    "no-mixed-operators": 0,
-    "no-param-reassign": 0,
-    "no-plusplus": [2, {"allowForLoopAfterthoughts": true}],
-    "import/no-extraneous-dependencies": 0,
-    "max-len": [2, {"code": 160}],
-    "import/no-unresolved": 0,
-    "import/extensions": 0,
-    "default-case": 0,
-    "space-infix-ops": ["error", {"int32Hint": false}]
-  }
+    "parser": "babel-eslint",
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "airbnb-base",
+    "rules": {
+        "no-mixed-operators": 0,
+        "no-param-reassign": 0,
+        "no-plusplus": [2, {"allowForLoopAfterthoughts": true}],
+        "import/no-extraneous-dependencies": 0,
+        "max-len": [2, {"code": 160}],
+        "import/no-unresolved": 0,
+        "import/extensions": 0,
+        "default-case": 0,
+        "space-infix-ops": ["error", {"int32Hint": false}],
+        "indent": ["error", 4],
+        "linebreak-style": 0,
+    }
 };
--- src/utils/motionEstimation.js
+++ src/utils/motionEstimation.js
@@ -3,51 +3,13 @@ import jsfeat from 'jsfeat';
 jsfeat.fast_corners.set_threshold(20);
 
 /**
-* Calculates the relative camera pose using two frames
-* 1. Convert frames to grayscale
-* 2. Find feature in previous frame
-* 3. Map features in previous frame to current frame
-* 4. Use 8 point algorithm with ransac to find essential matrix
-* 5. Compute R and T from essential matrix
-**/
-function motionEstimation(prevFrame, currentFrame, width, height) {
-    // convert frames to grayscale
-    const prevPyramidT = convertToGrayScale(prevFrame, width, height);
-    const currentPyramidT = convertToGrayScale(currentFrame, width, height);
-
-    const prevFeatures = [];
-    const currentFeatures = [];
-    // detect features for previous frame
-    const featuresCount = detectFeatures(prevPyramidT, prevFeatures, width, height);
-
-    // klt tracker - tracks features in previous img and maps them to current
-    const status = [];
-    jsfeat.optical_flow_lk.track(prevPyramidT, currentPyramidT,
-    prevFeatures, currentFeatures, featuresCount,
-    15, 30, status, 0.01, 0.0001);
-
-    // ransac with 8 point algorithm
-    const ransac = jsfeat.motion_estimator.ransac;
-    // create homography kernel
-    const homo_kernel = new jsfeat.motion_model.homography2d();
-    const essentialMatrix = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
-    const params = new jsfeat.ransac_params_t(4, 3, 0.5, 0.99);
-
-    // calculate essential matrix using features detected in the two images
-    ransac(params, homo_kernel, prevFeatures, currentFeatures, featuresCount, essentialMatrix, status, 1000);
-
-    // return rotation and translation calculated from essentialMatrix
-    return recoverPose(essentialMatrix, currentFeatures, prevFeatures);
-};
-
-/**
 * Converts frame to grayscale
 * @return pyramidT : pyramid_t
-**/
+*/
 function convertToGrayScale(frame, width, height) {
     // allocate pyramid data structure for feature tracker
     const pyramidT = new jsfeat.pyramid_t(3);
-    pyramidT.allocate(width, height, jsfeat.U8_t | jsfeat.C1_t);
+    pyramidT.allocate(width, height, jsfeat.U8_t);
 
     // get grayscale version of frames
     jsfeat.imgproc.grayscale(frame, width, height, pyramidT.data[0], jsfeat.COLOR_RGBA2GRAY);
@@ -56,12 +18,12 @@ function convertToGrayScale(frame, width, height) {
     pyramidT.build(pyramidT.data[0], false);
 
     return pyramidT;
-};
+}
 
 /**
 * Detect features in frame using fast algorithm
 * @return number of features detected
-**/
+*/
 function detectFeatures(frame, features, width, height) {
     // detect features for frame
     const tempFeatures = [];
@@ -77,29 +39,41 @@ function detectFeatures(frame, features, width, height) {
     }
 
     return count;
-};
+}
+
+/**
+* Converts a 3x3 rotation matrix in array format to x/y/z angles
+* Uses equation found here http://nghiaho.com/?page_id=846
+*/
+function decomposeRotationMatrix(matrix) {
+    return {
+        x: Math.atan2(matrix[7], matrix[8]),
+        y: Math.atan2(-matrix[6], Math.sqrt((matrix[7] ** 2) + (matrix[8] ** 2))),
+        z: Math.atan2(matrix[3], matrix[0]),
+    };
+}
 
 /** Calculates the rotation and translation using an essential matrix
 * E = U * D * V after svd
 * R = U * Winvert * V
 * T = U * W * D * Ut
-**/
-function recoverPose(essentialMatrix, currentFeatures, prevFeatures) {
-    const D = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
-    const U = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
-    const V = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
-    const W = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
+*/
+function recoverPose(essentialMatrix) {
+    const D = new jsfeat.matrix_t(3, 3, jsfeat.F32_t);
+    const U = new jsfeat.matrix_t(3, 3, jsfeat.F32_t);
+    const V = new jsfeat.matrix_t(3, 3, jsfeat.F32_t);
+    const W = new jsfeat.matrix_t(3, 3, jsfeat.F32_t);
     W.data[1] = -1;
     W.data[3] = 1;
     W.data[8] = 1;
 
     // get inverse of W
-    const Winvert = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
+    const Winvert = new jsfeat.matrix_t(3, 3, jsfeat.F32_t);
     jsfeat.linalg.svd_invert(Winvert, W);
 
     // rotation and translation matrices
-    const R = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
-    const T = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
+    const R = new jsfeat.matrix_t(3, 3, jsfeat.F32_t);
+    const T = new jsfeat.matrix_t(3, 3, jsfeat.F32_t);
 
     // calculate SVD of essential matrix
     jsfeat.linalg.svd_decompose(essentialMatrix, D, U, V);
@@ -117,18 +91,45 @@ function recoverPose(essentialMatrix, currentFeatures, prevFeatures) {
         rotation: decomposeRotationMatrix(R.data),
         translation: T.data,
     };
-};
+}
 
 /**
-* Converts a 3x3 rotation matrix in array format to x/y/z angles
-* Uses equation found here http://nghiaho.com/?page_id=846
-**/
-function decomposeRotationMatrix(matrix) {
-    return {
-        x: Math.atan2(matrix[7], matrix[8]),
-        y: Math.atan2(-matrix[6], Math.sqrt(Math.pow(matrix[7], 2) + Math.pow(matrix[8], 2))),
-        z: Math.atan2(matrix[3], matrix[0]),
-    };
-};
+* Calculates the relative camera pose using two frames
+* 1. Convert frames to grayscale
+* 2. Find feature in previous frame
+* 3. Map features in previous frame to current frame
+* 4. Use 8 point algorithm with ransac to find essential matrix
+* 5. Compute R and T from essential matrix
+*/
+function motionEstimation(prevFrame, currentFrame, width, height) {
+    // convert frames to grayscale
+    const prevPyramidT = convertToGrayScale(prevFrame, width, height);
+    const currentPyramidT = convertToGrayScale(currentFrame, width, height);
+
+    const prevFeatures = [];
+    const currentFeatures = [];
+    // detect features for previous frame
+    const featuresCount = detectFeatures(prevPyramidT, prevFeatures, width, height);
+
+    // klt tracker - tracks features in previous img and maps them to current
+    const status = [];
+    jsfeat.optical_flow_lk.track(
+        prevPyramidT, currentPyramidT, prevFeatures, currentFeatures,
+        featuresCount, 15, 30, status, 0.01, 0.0001,
+    );
+
+    // ransac with 8 point algorithm
+    const ransac = jsfeat.motion_estimator.ransac;
+    // create homography kernel
+    const homo_kernel = new jsfeat.motion_model.homography2d();
+    const essentialMatrix = new jsfeat.matrix_t(3, 3, jsfeat.F32_t);
+    const params = new jsfeat.ransac_params_t(4, 3, 0.5, 0.99);
+
+    // calculate essential matrix using features detected in the two images
+    ransac(params, homo_kernel, prevFeatures, currentFeatures, featuresCount, essentialMatrix, status, 1000);
+
+    // return rotation and translation calculated from essentialMatrix
+    return recoverPose(essentialMatrix);
+}
 
 export default motionEstimation;
