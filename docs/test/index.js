b664f5ed4b2345aa1d4d8082e4fbf39fec82195f
lunzhang <lun.zhang1993@gmail.com>
2017-11-30 15:20:03 -0500

    [feature] add test page to docs

---
+++ docs/test/index.js
@@ -0,0 +1,130 @@
+const IMAGE_WIDTH = 300;
+const IMAGE_HEIGHT = 200;
+
+// Draw image to canvas
+function addImage(canvasId, imageSrc) {
+    return new Promise(function(res, rej) {
+        var canvas = document.getElementById(canvasId);
+        var context = canvas.getContext('2d');
+        var image = new Image();
+        image.src = imageSrc;
+
+        image.onload = function() {
+            canvas.width = IMAGE_WIDTH;
+            canvas.height = IMAGE_HEIGHT;
+            context.drawImage(image, 0, 0, IMAGE_WIDTH, IMAGE_HEIGHT);
+            var frame = context.getImageData(0, 0, IMAGE_WIDTH, IMAGE_HEIGHT);
+            res(frame);
+        };
+    });
+}
+
+// Computes the grayscale of an images
+// Renders the grayscale image to canvas
+function drawGrayScale(canvasId, frame) {
+    return new Promise(function(res, rej) {
+        var canvas = document.getElementById(canvasId);
+        var context = canvas.getContext('2d');
+        canvas.width = IMAGE_WIDTH;
+        canvas.height = IMAGE_HEIGHT;
+        var grayScale = convertToGrayScale(frame.data, IMAGE_WIDTH, IMAGE_HEIGHT);
+        var grayImg = grayScale.data[0].data;
+        var imageData = new ImageData(IMAGE_WIDTH, IMAGE_HEIGHT);
+        var c = 0;
+        /**
+         * Since image data is in RGBA, and grayImg is just one pixel
+         * R = G = B = A for each pixel in grayImg
+         */
+        for(var i = 0; i < imageData.data.length; i += 4) {
+            imageData.data[i] = grayImg[c];
+            imageData.data[i + 1] = grayImg[c];
+            imageData.data[i + 2] = grayImg[c];
+            imageData.data[i + 3] = grayImg[c];
+            c++;
+        }
+        context.putImageData(imageData, 0, 0);
+
+        res(grayScale);
+    });
+}
+
+/**
+* Detect image1 features and renders them to canvas1
+* Tracks image2 features and renders them to canvas2
+*/
+function detectAndTrackFeatures(canvas1Id, canvas2Id, frame1, frame2) {
+    return new Promise(function(res, rej) {
+        var canvas1 = document.getElementById(canvas1Id);
+        var context1 = canvas1.getContext('2d');
+        context1.fillStyle = "#ff0000";
+        var canvas2 = document.getElementById(canvas2Id);
+        var context2 = canvas2.getContext('2d');
+        context2.fillStyle = "#ff0000";
+        var frame1Feat = [];
+        var frame2Feat = [];
+
+        // Detect and draw image1 features
+        var featuresCount = detectFeatures(frame1, frame1Feat, IMAGE_WIDTH, IMAGE_HEIGHT);
+        for(var i = 0; i < frame1Feat.length; i += 2) {
+            context1.beginPath();
+            context1.arc(frame1Feat[i], frame1Feat[i + 1], 1, 0, 2*Math.PI);
+            context1.fill();
+        }
+
+        // track image2 features
+        var status = [];
+        jsfeat.optical_flow_lk.track(
+            frame1, frame2, frame1Feat, frame2Feat,
+            featuresCount, 15, 30, status, 0.01, 0.0001,
+        );
+
+        // Draw image 2 features
+        for(var i = 0; i < frame1Feat.length; i += 2) {
+            context2.beginPath();
+            context2.arc(frame2Feat[i], frame2Feat[i + 1], 1, 0, 2*Math.PI);
+            context2.fill();
+        }
+
+        res({frame1Feat, frame2Feat, featuresCount, status});
+    });
+}
+
+// calculate camera pose using frame features
+function calculateCameraPose(frame1Feat, frame2Feat, featuresCount, status) {
+    var text = document.getElementById('camera-pose');
+
+    // ransac with 8 point algorithm
+    var ransac = jsfeat.motion_estimator.ransac;
+    // create homography kernel
+    var homo_kernel = new jsfeat.motion_model.homography2d();
+    var essentialMatrix = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
+    var params = new jsfeat.ransac_params_t(4, 3, 0.5, 0.99);
+
+    // calculate essential matrix using features detected in the two images
+    ransac(params, homo_kernel, frame1Feat, frame2Feat, featuresCount, essentialMatrix, status, 1000);
+
+    var pose = recoverPose(essentialMatrix);
+
+    text.innerHTML = 'Rotation X: ' + pose.rotation.x + '\n';
+    text.innerHTML += 'Rotation Y: ' + pose.rotation.y + '\n';
+    text.innerHTML += 'Rotation Z: ' + pose.rotation.z + '\n';
+    text.innerHTML += 'Translation X: ' + pose.translation.x + '\n';
+    text.innerHTML += 'Translation Y: ' + pose.translation.y + '\n';
+    text.innerHTML += 'Translation Z: ' + pose.translation.z + '\n';
+}
+
+// Draw image 2 first
+addImage('original-image-2', '../res/test2.jpg').then(function(frame) {
+    drawGrayScale('grayscale-image-2', frame);
+    return drawGrayScale('feature-image-2', frame);
+}).then(function(frame2) {
+    // Draw image 1
+    addImage('original-image-1', '../res/test1.jpg').then(function(frame1) {
+        drawGrayScale('grayscale-image-1', frame1);
+        return drawGrayScale('feature-image-1', frame1);
+    }).then(function(frame1) {
+        return detectAndTrackFeatures('feature-image-1', 'feature-image-2', frame1, frame2);
+    }).then(function({frame1Feat, frame2Feat, featuresCount, status}) {
+        return calculateCameraPose(frame1Feat, frame2Feat, featuresCount, status);
+    });
+});
---
+++ docs/test/motionEstimation.js
@@ -0,0 +1,131 @@
+jsfeat.fast_corners.set_threshold(20);
+
+/**
+* Converts frame to grayscale
+* @return pyramidT : pyramid_t
+*/
+function convertToGrayScale(frame, width, height) {
+    // allocate pyramid data structure for feature tracker
+    const pyramidT = new jsfeat.pyramid_t(3);
+    pyramidT.allocate(width, height, jsfeat.U8_t | jsfeat.C1_t);
+
+    // get grayscale version of frames
+    jsfeat.imgproc.grayscale(frame, width, height, pyramidT.data[0], jsfeat.COLOR_RGBA2GRAY);
+
+    // build pyramid layers
+    pyramidT.build(pyramidT.data[0], false);
+
+    return pyramidT;
+}
+
+/**
+* Detect features in frame using fast algorithm
+* @return number of features detected
+*/
+function detectFeatures(frame, features, width, height) {
+    // detect features for frame
+    const tempFeatures = [];
+    for (let i = 0; i < width * height; i++) {
+        tempFeatures[i] = new jsfeat.keypoint_t();
+    }
+    const count = jsfeat.fast_corners.detect(frame.data[0], tempFeatures, 3);
+
+    // add detected features to array
+    for (let i = 0; i < count; i++) {
+        features.push(tempFeatures[i].x);
+        features.push(tempFeatures[i].y);
+    }
+
+    return count;
+}
+
+/**
+* Converts a 3x3 rotation matrix in array format to x/y/z angles
+* Uses equation found here http://nghiaho.com/?page_id=846
+*/
+function decomposeRotationMatrix(matrix) {
+    return {
+        x: Math.atan2(matrix[7], matrix[8]),
+        y: Math.atan2(-matrix[6], Math.sqrt((matrix[7] ** 2) + (matrix[8] ** 2))),
+        z: Math.atan2(matrix[3], matrix[0]),
+    };
+}
+
+/** Calculates the rotation and translation using an essential matrix
+* E = U * D * V after svd
+* R = U * Winvert * V
+* T = U * W * D * Ut
+*/
+function recoverPose(essentialMatrix) {
+    const D = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
+    const U = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
+    const V = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
+    const W = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
+    W.data[1] = -1;
+    W.data[3] = 1;
+    W.data[8] = 1;
+
+    // get inverse of W
+    const Winvert = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
+    jsfeat.linalg.svd_invert(Winvert, W);
+
+    // rotation and translation matrices
+    const R = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
+    const T = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
+
+    // calculate SVD of essential matrix
+    jsfeat.linalg.svd_decompose(essentialMatrix, D, U, V);
+
+    // R = U * Winvert * V
+    jsfeat.matmath.multiply_3x3(R, U, Winvert);
+    jsfeat.matmath.multiply_3x3(R, R, V);
+
+    // T = U * W * D * Ut
+    jsfeat.matmath.multiply_3x3(T, U, W);
+    jsfeat.matmath.multiply_3x3(T, T, D);
+    jsfeat.matmath.multiply_ABt(T, T, U);
+
+    return {
+        rotation: decomposeRotationMatrix(R.data),
+        translation: T.data,
+    };
+}
+
+/**
+* Calculates the relative camera pose using two frames
+* 1. Convert frames to grayscale
+* 2. Find feature in previous frame
+* 3. Map features in previous frame to current frame
+* 4. Use 8 point algorithm with ransac to find essential matrix
+* 5. Compute R and T from essential matrix
+*/
+function motionEstimation(prevFrame, currentFrame, width, height) {
+    // convert frames to grayscale
+    const prevPyramidT = convertToGrayScale(prevFrame, width, height);
+    const currentPyramidT = convertToGrayScale(currentFrame, width, height);
+
+    const prevFeatures = [];
+    const currentFeatures = [];
+    // detect features for previous frame
+    const featuresCount = detectFeatures(prevPyramidT, prevFeatures, width, height);
+
+    // klt tracker - tracks features in previous img and maps them to current
+    const status = [];
+    jsfeat.optical_flow_lk.track(
+        prevPyramidT, currentPyramidT, prevFeatures, currentFeatures,
+        featuresCount, 15, 30, status, 0.01, 0.0001,
+    );
+
+    // ransac with 8 point algorithm
+    const ransac = jsfeat.motion_estimator.ransac;
+    // create homography kernel
+    const homo_kernel = new jsfeat.motion_model.homography2d();
+    const essentialMatrix = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
+    const params = new jsfeat.ransac_params_t(4, 3, 0.5, 0.99);
+
+    // calculate essential matrix using features detected in the two images
+    ransac(params, homo_kernel, prevFeatures, currentFeatures, featuresCount, essentialMatrix, status, 1000);
+
+    // return rotation and translation calculated from essentialMatrix
+    return recoverPose(essentialMatrix);
+}
