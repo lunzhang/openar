129687d7c2f71cd294f21a4dddaa05c7aa35093f
lunzhang <lun.zhang1993@gmail.com>
2018-01-02 12:51:48 -0500

    Added ransac to Essential Matrix calc

--- src/utils/5point/computeEssential.js
+++ src/utils/5point/computeEssential.js
@@ -1,16 +1,14 @@
 import compute_e_matrices from './Ematrix_5pt';
 
-const MAX_ITERATIONS = 1;
+const MAX_ITERATIONS = 100;
 const MODEL_SIZE = 5;
 const PROBABILITY = 0.99;
 const focal = 800;
 const IMAGE_WIDTH = 150;
 const IMAGE_HEIGHT = 150;
-const cameraParams = [
-    [focal, 0, IMAGE_WIDTH / 2],
-    [0,	focal, IMAGE_HEIGHT / 2],
-    [0, 0, 1]
-];
+const cameraParams = [[focal, 0, IMAGE_WIDTH / 2],
+[0,	focal, IMAGE_HEIGHT / 2],
+[0, 0, 1]];
 
 // nX = x - px / 2
 function normalizeXCoord(xCoord) {
@@ -26,79 +24,105 @@ function normalizeYCoord(yCoord) {
     return Ycoordinate;
 };
 
-// E = K1t * F * K2 -> F = E * K'1t * K'2
-function getFundamentalMat(eMat) {
-    let fMat = [];
-
-    const K = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
-    const Kt = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
-    const F = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
-    const E = new jsfeat.matrix_t(3, 3, jsfeat.F32_t | jsfeat.C1_t);
-
-    E.data = eMat[0].concat(eMat[1].concat(eMat[2]));
-    K.data = cameraParams[0].concat(cameraParams[1].concat(cameraParams[2]));
-    jsfeat.matmath.transpose(Kt, K);
-    jsfeat.matmath.invert_3x3(K, K);
-    jsfeat.matmath.invert_3x3(Kt, Kt);
-
-    jsfeat.matmath.multiply_3x3(F, E, K);
-    jsfeat.matmath.multiply_3x3(F, F, Kt);
-
-    for(let i = 0; i < 3; i++) {
-        fMat.push([F.data[i * 3], F.data[i * 3 + 1], F.data[i * 3 + 2]]);
-    }
-
-    return fMat;
-};
-
 function computeEssential(feature1, feature2) {
-    // 3x3 matrix;
-    const essentialMatrix = [];
-    for (let i = 0; i < 3; i++) {
-        essentialMatrix.push([]);
-    }
+    let essentialMatrix;
+    let maxInliers;
 
     const feats1 = [];
     const feats2 = [];
-
-    // normalize coordinates
-    for (let i = 0; i < feature1.length; i++) {
-        feats1.push(normalizeXCoord(feature1[i * 2]));
-        feats1.push(normalizeYCoord(feature1[i * 2 + 1]));
-        feats2.push(normalizeXCoord(feature2[i * 2]));
-        feats2.push(normalizeYCoord(feature2[i * 2 + 1]));
+    for (let i = 0; i < feature1.length; i+=2) {
+        feats1.push(normalizeXCoord(feature1[i]));
+        feats1.push(normalizeYCoord(feature1[i + 1]));
+        feats2.push(normalizeXCoord(feature2[i]));
+        feats2.push(normalizeYCoord(feature2[i + 1]));
     }
 
     let iterations = 0;
-
     while(iterations < MAX_ITERATIONS) {
-        let points1 = [];
-        let points2 = [];
+        const points1 = [];
+        const points2 = [];
+        const indexDict = {};
 
         // Select 5 random points
         for (let i = 0; i < 5; i++) {
             let index = Math.floor(Math.random() * (feature1.length / 2));
-            let point1 = [feats1[i * 2], feats1[i * 2 + 1]];
-            let point2 = [feats2[i * 2], feats2[i * 2 + 1]];
+
+            // avoid picking same points
+            while(indexDict[index]) {
+                index = Math.floor(Math.random() * (feature1.length / 2));
+            }
+
+            let point1 = [feats1[index * 2], feats1[index * 2 + 1]];
+            let point2 = [feats2[index * 2], feats2[index * 2 + 1]];
             points1.push(point1);
             points2.push(point2);
+
+            indexDict[index] = true;
         }
 
+        // compute the essential matrix
         const { Ematrices, nroots } = compute_e_matrices(points1, points2);
 
+        // if no roots are found, avoid unncessary steps
+        if(nroots === 0) continue;
+
         let E;
+        let minError;
+        /** gets the eMat with the lowest error
+         *  X'EX = 0 or in our case the error
+         * [x', x', 1] * E * [x, x, 1] = 0
+         * Found here https://www.cc.gatech.edu/~hays/compvision/proj3/
+        **/
         for (let i = 0; i < nroots; i++) {
-            let fMat = getFundamentalMat(Ematrices[i]);
-            // x'Fx = 0 or close to 0
-            let error = (fMat[0][0] * feature1[0] * feature2[0]) +
-            (fMat[0][1] * feature1[1] * feature2[0]) +
-            (fMat[0][2] * feature2[0]) +
-            (fMat[1][0] * feature1[0] * feature2[1]) +
-            (fMat[1][1] * feature1[1] * feature2[1]) +
-            (fMat[1][2] * feature2[1]) +
-            (fMat[2][0] * feature1[0]) +
-            (fMat[2][1] * feature1[1]) +
-            (fMat[2][2]);
+            let eMat = Ematrices[i];
+
+            /**
+             *  We test with first points out of the 5 random
+             *  Matrix with the lowest error is picked
+            **/
+            let error = (eMat[0][0] * points1[0][0] * points2[0][0]) +
+            (eMat[0][1] * points1[0][1] * points2[0][0]) +
+            (eMat[0][2] * points2[0][0]) +
+            (eMat[1][0] * points1[0][0] * points2[0][1]) +
+            (eMat[1][1] * points1[0][1] * points2[0][1]) +
+            (eMat[1][2] * points2[0][1]) +
+            (eMat[2][0] * points1[0][0]) +
+            (eMat[2][1] * points1[0][1]) +
+            (eMat[2][2]);
+
+            error = Math.abs(error);
+
+            if (minError === undefined) {
+                minError = error;
+                E = eMat;
+            } else if(error < minError){
+                minError = error;
+                E = eMat;
+            }
+        }
+
+        // Count number of inliers
+        let inliers = 0;
+        for(let i = 0; i < feats1.length; i+=2) {
+            let error = (E[0][0] * feats1[i] * feats2[i]) +
+            (E[0][1] * feats1[i + 1] * feats2[i]) +
+            (E[0][2] * feats2[i]) +
+            (E[1][0] * feats1[i] * feats2[i + 1]) +
+            (E[1][1] * feats1[i + 1] * feats2[i + 1]) +
+            (E[1][2] * feats2[i + 1]) +
+            (E[2][0] * feats1[i]) +
+            (E[2][1] * feats1[i + 1]) +
+            (E[2][2]);
+
+            error = Math.abs(error);
+
+            if (error < 1e-10) inliers++;
+        }
+
+        // Set essential if first computed or one with greater inliers
+        if (maxInliers === undefined || inliers > maxInliers) {
+            maxInliers = inliers;
+            essentialMatrix = E;
         }
 
         iterations++;
@@ -106,6 +130,5 @@ function computeEssential(feature1, feature2) {
 
     return essentialMatrix;
 }
-
 
 export default computeEssential;
